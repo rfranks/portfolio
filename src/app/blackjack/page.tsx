8d2b006f83c78d92d9c8af6ea21cc6e016d45c21
Richard Franks <richardfranksjr@hotmail.com>
2025-08-22 21:08:08 -0400

    feat(blakcjack): Cleanup and migrate to Next.js page.

---
+++ src/app/blackjack/page.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import * as React from "react";
+import "./page.css";
+
+export default function BlackjackPage() {
+  return (
+    <>
+      <div id="game">
+        <div id="dealer">
+          <div id="dealer-cards" className="cards"></div>
+        </div>
+        <div id="player">
+          <div id="player-cards" className="cards"></div>
+        </div>
+        <div id="status"></div>
+      </div>
+      <div id="controls">
+        <button id="deal">Deal</button>
+        <button id="hit">Hit</button>
+        <button id="stand">Stand</button>
+        <button id="double">Double</button>
+        <button id="split">Split</button>
+        <button id="insure" style={{ display: "none" }}>
+          Insure
+        </button>
+        <button id="decline" style={{ display: "none" }}>
+          Decline
+        </button>
+      </div>
+      <script src="wasm_exec.js"></script>
+      <script id="wasm" src="main.wasm" type="application/wasm"></script>
+      <script src="main.js"></script>
+    </>
+  );
+}
