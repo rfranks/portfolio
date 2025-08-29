b3aa271b170adb931146ad1047acf36c25081c28
rfrankspk <rfrankspk@users.noreply.github.com>
2025-08-29 20:49:53 +0000

    deploy: 7208c39254c36314b657043d3e19b280ebfe7113

--- _next/static/chunks/48be2b09bf7872f6.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_error", [
-  "static/chunks/d834e751564bb2f1.js",
-  "static/chunks/7e1cfc83e728e150.js",
-  "static/chunks/turbopack-c394d20f717448d4.js"
-])
---
+++ _next/static/eTREMsBIvpwA52bFWpHOr/_buildManifest.js
@@ -0,0 +1,14 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/b2c04574228acc98.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
