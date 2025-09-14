4fd8cf42ff714bd4fdafafbae6be9d05308e3464
Richard Franks <richardfranksjr@hotmail.com>
2025-09-14 01:15:15 -0400

    Add placeholder pages for TalentForge sections

---
+++ src/app/talentforge/offers/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import OfferCompare from "@/components/talentforge/OfferCompare";
+
+export default function OffersPage() {
+  return <OfferCompare />;
+}
+
---
+++ src/app/talentforge/resumes/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import ResumeManager from "@/components/talentforge/ResumeManager";
+
+export default function ResumesPage() {
+  return <ResumeManager />;
+}
+
