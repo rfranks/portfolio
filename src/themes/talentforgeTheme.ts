9bb17f3007fb6999f38380fe131061528655c700
Richard Franks <richardfranksjr@hotmail.com>
2025-09-14 00:48:09 -0400

    Add talentforge theme and layout with sidebar

---
+++ src/themes/talentforgeTheme.ts
@@ -0,0 +1,6 @@
+import { createTheme } from '@mui/material/styles';
+import getLPTheme from './talentforge/theme';
+
+const talentforgeTheme = createTheme(getLPTheme('light'));
+
+export default talentforgeTheme;
