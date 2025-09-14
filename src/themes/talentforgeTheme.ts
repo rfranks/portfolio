27ff8aca11381078560a03eb253836708c83cbeb
Richard Franks <richardfranksjr@hotmail.com>
2025-09-14 02:55:13 -0400

    Add TalentForge layout shell and theme

--- src/themes/talentforgeTheme.ts
+++ src/themes/talentforgeTheme.ts
@@ -1,6 +1,19 @@
-import { createTheme } from '@mui/material/styles';
-import getLPTheme from './talentforge/theme';
+import { PaletteMode, createTheme } from "@mui/material";
 
-const talentforgeTheme = createTheme(getLPTheme('light'));
+// Create a theme for TalentForge with custom breakpoints
+// and dynamic light/dark palette mode.
+const talentforgeTheme = (mode: PaletteMode) =>
+  createTheme({
+    palette: { mode },
+    breakpoints: {
+      values: {
+        xs: 0,
+        sm: 600,
+        md: 900,
+        lg: 1200,
+        xl: 1536,
+      },
+    },
+  });
 
 export default talentforgeTheme;
