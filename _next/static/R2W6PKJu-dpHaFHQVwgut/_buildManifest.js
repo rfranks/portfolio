7efbea228a7e5e4fe4f6563ad83bf8cfe6354178
rfranks <rfranks@users.noreply.github.com>
2026-03-17 15:09:07 +0000

    deploy: 7e74176f68b73779fb1d6b3425dce02db473725d

---
+++ _next/static/R2W6PKJu-dpHaFHQVwgut/_buildManifest.js
@@ -0,0 +1,14 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/08361b66a8c6322c.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
